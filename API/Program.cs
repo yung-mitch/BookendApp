89a67c69241553250e7aeef67728a99ed7fd4b5d
Mitchell Eldridge <mitchell@focusbf.com>
2023-12-30 08:00:38 -0600

    MSE0003
    12/30/2023
    Creates Data Context Class from EF DbContext and Injects Into Program.cs
    API/Data/DataContext.cs
      Creates DataContext class which inherits from EF Core's DbContext
      class
      Adds Users DbSet from AppUser entity
    API/Entities/AppUser.cs
      Creates AppUser entity
    API/Program.cs
      Injects DataContext into the aplication builder with connection string
      to local Sqlite database
    API/appsettings.Development.json
      Adds connection string to local Sqlite database - for development
      Sqlite database is only one file

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
