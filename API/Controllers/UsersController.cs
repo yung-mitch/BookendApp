5af5c88b97d2c35a8c3400bcf7c29fd44ffd3d64
Mitchell Eldridge <mitchell@focusbf.com>
2023-12-30 08:13:40 -0600

    MSE0004
    12/30/2023
    Adds Users Controller to create API endpoints
    API/Controllers/UsersController.cs
      Creates Users Controller
      Creates two endpoints - one to get all Users and one to get one User
      by its Id

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+            public async Task<ActionResult<AppUser>> GetUser(int id)
+            {
+                return await _context.Users.FindAsync(id);
+            }
+    }
+}
