4e662659700a33d305269357ad63e2f654619804
Mitchell Eldridge <mitchell@focusbf.com>
2024-01-23 07:39:16 -0600

    MSE0018
    1/23/24
    
    Creates the Nav Component
    
    bookend_client/src/app/app.component.html
      Adds nav component inside app component
    bookend_client/src/app/app.module.ts
      Adds Home and Nav components to app module
    bookend_client/src/app/nav/nav.component.css
      Creates nav componet css
    bookend_client/src/app/nav/nav.component.html
      Creates nav component template
    bookend_client/src/app/nav/nav.component.spec.ts
      Creates nav component spec file
    bookend_client/src/app/nav/nav.component.ts
      Creates nav component

--- bookend_client/src/app/app.module.ts
+++ bookend_client/src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
