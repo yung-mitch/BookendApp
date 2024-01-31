7fd699b6e1eb8fddb5446f5aad34db12346c1035
Mitchell Eldridge <mitchell@focusbf.com>
2024-01-31 10:51:02 -0600

    MSE0025
    1/31/24
    Changes database provider from SQLite to PostgreSQL to comply with
    deployment platform requirements; Prepares angular and API applications
    for production deployment
    
    .github/workflows/docker-push.yml
     Creates yml file for running Github Action
    .vscode/launch.json
    .vscode/tasks.json
     Build files
    API/.dockerignore
     Ignores /bin and /obj folders for docker image
    API/API.csproj
     Changes database provider dependency from SQLite to PostgreSQL
    API/Controllers/FallbackController.cs
     Creates Fallback Controller for production to tell app where the ng
     build files are located
    API/Dockerfile
     Creates docker script for building the docker container and runtime
     image
    API/Program.cs
     Changes database connection string to use PostgreSQL
     Tells the API app where the ng build files are located
     Tells the API app about the Fallback Controller
    API/wwwroot/3rdpartylicenses.txt
    API/wwwroot/favicon.ico
    API/wwwroot/fontawesome-webfont.2b13baa7dd4f54c9.eot
    API/wwwroot/fontawesome-webfont.8a7cb27d142e3e19.ttf
    API/wwwroot/fontawesome-webfont.cf011583fb81df9f.woff
    API/wwwroot/fontawesome-webfont.da909aa098b0ee2d.svg
    API/wwwroot/fontawesome-webfont.e9955780856cf8aa.woff2
    API/wwwroot/index.html
    API/wwwroot/main.c20a5787056e991e.js
    API/wwwroot/polyfills.c9784f749c71a762.js
    API/wwwroot/runtime.513d784d8aef9e99.js
    API/wwwroot/styles.c8e3520dce93e8ac.css
     ng build files
    bookend_client/angular.json
     Configures where to put the ng build files "../API/wwwroot"
     Configures build filesize warning and error maximums
    fly.toml
     Creates fly.io app configuration file for the bookendapp machine

--- API/Program.cs
+++ API/Program.cs
@@ -15,10 +15,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options => 
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 builder.Services.AddCors();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -57,6 +53,34 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy("AdvertiserExperience", policy => policy.RequireRole("Advertiser"));
 });
 
+var connString = "";
+if (builder.Environment.IsDevelopment())
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else
+{
+    // Use connection string provided at runtime by FlyIO
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+    var updatedHost = pgHost.Replace("flycast", "internal");
+
+    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(connString);
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -67,7 +91,11 @@ app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("ht
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
 
 // Seed database
 using var scope = app.Services.CreateScope();
